5b8e2663bb15603537439d005f6ee3c3831bb694
ssskram <paul.marks@pittsburghpa.gov>
2018-04-04 15:44:22 -0400

    emptied accordion

--- ClientApp/components/SelfService.tsx
+++ ClientApp/components/SelfService.tsx
@@ -18,50 +18,63 @@ export class SelfService extends React.Component<RouteComponentProps<{}>, {}> {
                         <h3><strong>Self service</strong></h3>
                         <div>
                         <p>
-                        self-serv·ice
-                        ˈˌself ˈsərvəs/Submit
-                        adjective
-                        1.
-                        denoting a store, restaurant, or service station where customers select goods for themselves or service their car for themselves and pay a cashier.
-                        "a self-service cafeteria"
-                        
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
                         </p>
                         </div>
                         <h3>Hardware</h3>
                         <div>
                         <p>
-                        Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
-                        purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor
-                        velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In
-                        suscipit faucibus urna.
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
                         </p>
                         </div>
                         <h3>Software</h3>
                         <div>
                         <p>
-                        Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis.
-                        Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero
-                        ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis
-                        lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui.
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
                         </p>
                         <ul>
-                            <li>List item one</li>
-                            <li>List item two</li>
-                            <li>List item three</li>
+
                         </ul>
                         </div>
                         <h3>Workshops</h3>
                         <div>
                         <p>
-                        Cras dictum. Pellentesque habitant morbi tristique senectus et netus
-                        et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in
-                        faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia
-                        mauris vel est.
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
                         </p>
                         <p>
-                        Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus.
-                        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-                        inceptos himenaeos.
+
                         </p>
                         </div>
                     </div>
