2c2ec374ed843b0408cffdb5f2a8d06c26c3ae8b
ssskram <paul.marks@pittsburghpa.gov>
2018-04-04 22:52:52 -0400

    monoCHROME

--- ClientApp/components/SelfService.tsx
+++ ClientApp/components/SelfService.tsx
@@ -15,16 +15,26 @@ export class SelfService extends React.Component<RouteComponentProps<{}>, {}> {
             <div className="row">
                 <div className="col-md-12">
                     <div id="accordion">
-                        <h3><strong>Self service</strong></h3>
+                        <h3>Self service</h3>
                         <div>
                         <p>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
+                        <div className="def-title">
+                            self-serv·ice        
+                        </div>
+                        <div className="def-disamb">
+                            /ˈˌself ˈsərvəs/
+                        </div>
+                        <div className="def-adj">
+                            adjective
+                        </div>
+                        <div className="def">
+                            <ol>
+                                <li><p>of, for, or pertaining to something designed to be used or enjoyed without the aid of an attendant</p></li>
+                            </ol>
+                        </div>
+                        <hr/>
+                        <h2 className="text-center">These resources are for you</h2>
+                        <h4 className="text-center">if something is missing, please let us know</h4>
                         </p>
                         </div>
                         <h3>Hardware</h3>
@@ -56,7 +66,6 @@ export class SelfService extends React.Component<RouteComponentProps<{}>, {}> {
                         <br/>
                         </p>
                         <ul>
-
                         </ul>
                         </div>
                         <h3>Workshops</h3>
