244399da390f34ab7e801278ee902f4b23c2e0a8
ssskram <paul.marks@pittsburghpa.gov>
2018-06-21 10:26:18 -0400

    commentin stuff

--- ClientApp/boot-client.tsx
+++ ClientApp/boot-client.tsx
@@ -1,5 +1,6 @@
 import './css/site.css';
 import 'bootstrap';
+import 'react-table/react-table.css'
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
@@ -11,17 +12,12 @@ import { ApplicationState }  from './store';
 import * as RoutesModule from './routes';
 let routes = RoutesModule.routes;
 
-// Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
 const history = createBrowserHistory({ basename: baseUrl });
-
-// Get the application-wide store instance, prepopulating with state from the server where available.
 const initialState = (window as any).initialReduxState as ApplicationState;
 const store = configureStore(history, initialState);
 
 function renderApp() {
-    // This code starts up the React app when it runs in a browser. It sets up the routing configuration
-    // and injects the app into a DOM element.
     ReactDOM.render(
         <AppContainer>
             <Provider store={ store }>
@@ -34,7 +30,6 @@ function renderApp() {
 
 renderApp();
 
-// Allow Hot Module Replacement
 if (module.hot) {
     module.hot.accept('./routes', () => {
         routes = require<typeof RoutesModule>('./routes').routes;
