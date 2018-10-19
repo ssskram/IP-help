3e192cc68f9ac039bb27fc863432bd07caabb9f1
ssskram <paul.marks@pittsburghpa.gov>
2018-10-18 20:27:50 -0400

    el function

---
+++ ClientApp/components/EquipmentLoans/EquipmentLoans.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default class EquipmentLoans extends React.Component<any, any> {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+}
