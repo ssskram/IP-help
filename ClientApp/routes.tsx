8f106077d9c7fbfe0a581a822b64902d84298837
ssskram <paul.marks@pittsburghpa.gov>
2018-10-19 16:11:04 -0400

    fixed logout from modal

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import MobileDevice from './components/MobileDevice/MobileDevice';
-import PCOrder from './components/PCOrder/PCOrder';
-import EmployeeData from './components/EmployeeData/EmployeeData';
-import Other from './components/Other/Other';
-import { SelfService } from './components/SelfService';
-import Home from './components/Home';
+import * as React from 'react'
+import { Login } from './components/Account/Login'
+import { Route } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import MobileDevice from './components/MobileDevice/MobileDevice'
+import PCOrder from './components/PCOrder/PCOrder'
+import EmployeeData from './components/EmployeeData/EmployeeData'
+import Other from './components/Other/Other'
+import { SelfService } from './components/SelfService'
+import Home from './components/Home'
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
@@ -15,4 +16,5 @@ export const routes = <Layout>
     <Route path='/PCOrder' component={ PCOrder } />
     <Route path='/Other' component={ Other } />
     <Route path='/SelfService' component={ SelfService } />    
+    <Route path='/Account/Login' component={ Login } />
 </Layout>;
