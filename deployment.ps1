166b9720600fd05488ff8a50edc178b4c85ba76b
ssskram <paul.marks@pittsburghpa.gov>
2018-10-26 13:06:18 -0400

    gitignore

--- deployment.ps1
+++ deployment.ps1
@@ -1,8 +1,4 @@
 
-# first, set app settings with environment variables
-
-# next, commit
-
 git commit
 git push origin master
 az webapp deployment source sync --name IPHelp --slot IPhelp-staging --resource-group rescityofpitt
