2e3f1a23ef6dd293d7f3e86fdba98e7411b3d225
ssskram <paul.marks@pittsburghpa.gov>
2018-12-21 17:27:38 -0500

    last build before SANTA

--- app/src/home.tsx
+++ app/src/home.tsx
@@ -10,6 +10,9 @@ export default class Home extends React.Component<any, any> {
     private ref: React.RefObject<any>
     constructor(props) {
         super(props)
+        this.state = {
+            search: ''
+        }
         this.ref = React.createRef()
     }
 
@@ -49,85 +52,83 @@ export default class Home extends React.Component<any, any> {
                 </div>
                 <div ref={this.ref} className='home-container-2'>
                     <div className='home-child-2'>
-                        <div style={{ fontSize: '3em' }}>I need a...</div>
-                        <br />
-                        <div className='col-sm-6'>
-                            <Link to={'/NewPC'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>PC</span>
-                            </Link>
-                        </div>
-                        <div className='col-sm-6'>
-                            <Link to={'/NewPC'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Laptop</span>
-                            </Link>
-                        </div>
-                        <div className="clearfix"></div>
+                        <div className='row'>
+                            <div style={{ fontSize: '3em' }}>I need a...</div>
+                            <br />
+                            <div className='col-sm-6'>
+                                <Link to={'/NewPC'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>PC</span>
+                                </Link>
+                            </div>
+                            <div className='col-sm-6'>
+                                <Link to={'/NewPC'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Laptop</span>
+                                </Link>
+                            </div>
+                            <div className="clearfix"></div>
 
-                        <div className='col-sm-6'>
-                            <a className='btn btn-primary btn-home text-center' target='_blank' title="Request a new user account" href='https://cityofpittsburgh-my.sharepoint.com/:x:/r/personal/paul_scherrer_pittsburghpa_gov2/_layouts/15/WopiFrame.aspx?guestaccesstoken=Snz9oKYl15kxoHFHQPDs7rtciwH3mrGq7ZYGkIdqAOU%3d&docid=1_1f1cdb0766967490195afb496b2218146&wdFormId=%7B7F3E2D75-4E10-4612-BCE7-CFC4DA40381D%7D&action=formsubmit'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Email Address</span>
-                            </a>
-                        </div>
-                        <div className='col-sm-6'>
-                            <a className='btn btn-primary btn-home text-center' target='_blank' title="Request a new user account" href='https://cityofpittsburgh-my.sharepoint.com/:x:/r/personal/paul_scherrer_pittsburghpa_gov2/_layouts/15/WopiFrame.aspx?guestaccesstoken=Snz9oKYl15kxoHFHQPDs7rtciwH3mrGq7ZYGkIdqAOU%3d&docid=1_1f1cdb0766967490195afb496b2218146&wdFormId=%7B7F3E2D75-4E10-4612-BCE7-CFC4DA40381D%7D&action=formsubmit'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Network account</span>
-                            </a>
-                        </div>
-                        <div className="clearfix"></div>
+                            <div className='col-sm-6'>
+                                <a className='btn btn-primary btn-home text-center' target='_blank' title="Request a new user account" href='https://cityofpittsburgh-my.sharepoint.com/:x:/r/personal/paul_scherrer_pittsburghpa_gov2/_layouts/15/WopiFrame.aspx?guestaccesstoken=Snz9oKYl15kxoHFHQPDs7rtciwH3mrGq7ZYGkIdqAOU%3d&docid=1_1f1cdb0766967490195afb496b2218146&wdFormId=%7B7F3E2D75-4E10-4612-BCE7-CFC4DA40381D%7D&action=formsubmit'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Email Address</span>
+                                </a>
+                            </div>
+                            <div className='col-sm-6'>
+                                <a className='btn btn-primary btn-home text-center' target='_blank' title="Request a new user account" href='https://cityofpittsburgh-my.sharepoint.com/:x:/r/personal/paul_scherrer_pittsburghpa_gov2/_layouts/15/WopiFrame.aspx?guestaccesstoken=Snz9oKYl15kxoHFHQPDs7rtciwH3mrGq7ZYGkIdqAOU%3d&docid=1_1f1cdb0766967490195afb496b2218146&wdFormId=%7B7F3E2D75-4E10-4612-BCE7-CFC4DA40381D%7D&action=formsubmit'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Network account</span>
+                                </a>
+                            </div>
+                            <div className="clearfix"></div>
 
-                        <div className='col-sm-6'>
-                            <Link to={'/MobileDevice'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Cellphone</span><br />
-                            </Link>
-                        </div>
-                        <div className='col-sm-6'>
-                            <Link to={'/MobileDevice'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Tablet</span>
-                            </Link>
-                        </div>
-                        <div className="clearfix"></div>
-
-                        <div className='col-sm-6'>
-                            <Link to={'/Other'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Other</span>
-                            
-                            </Link>
+                            <div className='col-sm-6'>
+                                <Link to={'/MobileDevice'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Cellphone</span><br />
+                                </Link>
+                            </div>
+                            <div className='col-sm-6'>
+                                <Link to={'/MobileDevice'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Tablet</span>
+                                </Link>
+                            </div>
                         </div>
 
-                        <div style={{ fontSize: '3em', marginTop: '80px' }}>I want to...</div>
-                        <div className='col-sm-6'>
-                            <Link to={'/EquipmentLoan'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Borrow IT equipment</span>
-                            </Link>
+                        <div className='row'>
+                            <div style={{ fontSize: '3em', marginTop: '80px' }}>I want to...</div>
+                            <div className='col-sm-6'>
+                                <Link to={'/EquipmentLoan'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Borrow equipment</span>
+                                </Link>
+                            </div>
+                            <div className='col-sm-6'>
+                                <Link to={'/EquipmentDisposal'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Get rid of equipment</span>
+                                </Link>
+                            </div>
+                            <div className="clearfix"></div>
+                            <div className='col-sm-6'>
+                                <Link to={'/EquipmentDisposal'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Request employee data</span>
+
+                                </Link>
+                            </div>
                         </div>
-                        <div className='col-sm-6'>
-                            <Link to={'/EquipmentDisposal'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Get rid of IT equipment</span>
-                            </Link>
-                        </div>
-                        <div className="clearfix"></div>
-                        <div className='col-sm-6'>
-                            <Link to={'/EquipmentDisposal'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Request employee data</span>
-                            
-                            </Link>
-                        </div>
-                        <div className='col-sm-6'>
-                            <Link to={'/Other'} className='btn btn-primary btn-home text-center'>
-                                <img src={computer as string} className="center-block" />
-                                <span className='btn-home-header'>Other</span>
-                            
-                            </Link>
+
+                        <div className='row'>
+                            <div style={{ fontSize: '3em', marginTop: '80px' }}>Something else?</div>
+                            <div className='col-sm-12'>
+                                <Link to={'/Other'} className='btn btn-primary btn-home text-center'>
+                                    <img src={computer as string} className="center-block" />
+                                    <span className='btn-home-header'>Blank ticket</span>
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
