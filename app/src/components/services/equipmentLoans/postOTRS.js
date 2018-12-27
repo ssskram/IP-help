8c5c6a8b40e1d7a99429cb6cfa44dce0847db427
ssskram <paul.marks@pittsburghpa.gov>
2018-12-27 14:54:12 -0500

    passing equipment loan through sengrid factory for otrs, conrirmation

---
+++ app/src/components/services/equipmentLoans/postOTRS.js
@@ -0,0 +1,45 @@
+import sendgridPost from '../../../functions/sendgridEndpoint'
+
+export default async function postOTRS(request, user) {
+
+    // error handling
+    let success = true
+
+    // arguments for sendgrid
+    const args = {
+        to: undefined,
+        user: user,
+        subject: 'Equipment Loan #' + request.reservationID,
+        email: undefined,
+        attachment: undefined
+    }
+    const setBody = async (b) => args.email = b
+
+    // prepare emails
+    //.. first otrs
+    let otrsBody
+    await fetch('emailTemplates/equipmentLoanOTRS.html')
+        .then(response => response.text())
+        .then(text => otrsBody = String.format(text,
+            request.reservationID, // 0
+            user, // 1
+            request.items.map(u => u.item).join(', '), // 2
+            request.from, // 3
+            request.to)) // 4
+    await setBody(otrsBody)
+    success = await sendgridPost(args)
+
+    // ... then confirmation to user
+    let confirmationBody
+    await fetch('emailTemplates/equipmentLoanConfirmation.html')
+        .then(response => response.text())
+        .then(text => confirmationBody = String.format(text,
+            request.reservationID, // 0
+            request.types, // 1
+            request.from, // 2
+            request.to)) // 3
+    await setBody(confirmationBody)
+    success = await sendgridPost(args)
+
+    return postSuccess
+}
