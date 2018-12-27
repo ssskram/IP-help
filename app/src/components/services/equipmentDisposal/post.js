4d69f1a379f71ae3be5d41cf99731928e8ce8586
ssskram <paul.marks@pittsburghpa.gov>
2018-12-27 16:18:55 -0500

    spread spring.format all aroud

--- app/src/components/services/equipmentDisposal/post.js
+++ app/src/components/services/equipmentDisposal/post.js
@@ -35,3 +35,12 @@ export default async function postPickup(request, user) {
     const success = await sendgridPost(args)
     return success
 }
+
+String.format = function () {
+    var s = arguments[0]
+    for (var i = 0; i < arguments.length - 1; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm")
+        s = s.replace(reg, arguments[i + 1])
+    }
+    return s
+}
