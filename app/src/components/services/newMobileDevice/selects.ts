d0552db166ac74b639c3cac559795ec663407e1c
ssskram <paul.marks@pittsburghpa.gov>
2019-01-11 12:42:12 -0500

    updated mobile device selects

--- app/src/components/services/newMobileDevice/selects.ts
+++ app/src/components/services/newMobileDevice/selects.ts
@@ -1,11 +1,5 @@
 export const DeviceTypes = [
     { value: 'iPhone 7', label: 'iPhone 7' },
-    { value: 'iPhone SE', label: 'iPhone SE' },
     { value: 'Samsung Galaxy 8', label: 'Samsung Galaxy 8' },
-    { value: 'Kyocera DuraXV LTE (Flip phone)', label: 'Kyocera DuraXV LTE (Flip phone)' },
-    { value: 'Kyocera Dura Force Pro Rugged', label: 'Kyocera Dura Force Pro Rugged' },
-    { value: 'iPad Mini 7.9”', label: 'iPad Mini 7.9”' },
-    { value: 'iPad 10.5”', label: 'iPad 10.5”' },
-    { value: 'Samsung Galaxy Tab E 10”', label: 'Samsung Galaxy Tab E 10”' },
     { value: 'Jet pack', label: 'Jet pack' }
 ]
