bf25d5b4f944f9b3691f6c8e524552acae216d96
ssskram <paul.marks@pittsburghpa.gov>
2019-01-31 12:06:24 -0500

    bulk equipment to production, also tablet buttons redirects to PC order form

--- app/src/components/services/equipmentDisposal/markup/bulkOrSingle.tsx
+++ app/src/components/services/equipmentDisposal/markup/bulkOrSingle.tsx
@@ -4,13 +4,21 @@ type props = {
     setState: (stateObj: object) => void
 }
 
+const bigFont = {
+    fontSize: '1.5em'
+}
+
 export default class BulkOrSingle extends React.Component<props, {}> {
 
     public render() {
         return <div className='col-md-12 text-center'>
             <h3>How much equipment do you need picked up?</h3>
-            <button className='btn btn-primary' onClick={() => this.props.setState ({ bulk: false })}>A single item</button>
-            <button className='btn btn-primary' onClick={() => this.props.setState ({ bulk: true })}>Multiple items</button>
+            <button className='btn btn-primary' onClick={() => this.props.setState({ bulk: false })}>
+                <span style={bigFont}>A single item</span>
+            </button>
+            <button className='btn btn-primary' onClick={() => this.props.setState({ bulk: true })}>
+                <span style={bigFont}>Multiple items</span>
+            </button>
         </div>
     }
 }
