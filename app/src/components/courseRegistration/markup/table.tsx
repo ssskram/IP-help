00a07ea1bf8dd0d1d8d98ca72293eb1c1f43b097
ssskram <paul.marks@pittsburghpa.gov>
2019-02-20 16:50:12 -0500

    styling table

--- app/src/components/courseRegistration/markup/table.tsx
+++ app/src/components/courseRegistration/markup/table.tsx
@@ -13,7 +13,7 @@ type props = {
 export default class Table extends React.Component<props, {}> {
 
     viewEvent(event) {
-        this.props.setState ({
+        this.props.setState({
             viewCourse: true,
             course: this.props.courses.find(course => course.courseId == event.courseId)
         })
@@ -30,12 +30,20 @@ export default class Table extends React.Component<props, {}> {
         }, {
             Header: 'Capacity',
             accessor: 'maximumCapacity'
-        },{
+        }, {
             Header: 'Enrolled',
-            accessor: 'maximumCapacity'
-        },{
+            accessor: 'maximumCapacity',
+            Cell: props => {
+                const countActive = this.props.courseRegistrations.filter(reg => reg.courseCode == props.original.courseCode && reg.registrationStatus == "Active").length
+                return <div style={countActive >= props.original.maximumCapacity ? { color: 'red', fontWeight: 'bold' } : { color: 'green', fontWeight: 'bold' }}>{countActive}</div>
+            }
+        }, {
             Header: 'Waitlisted',
-            accessor: 'maximumCapacity'
+            accessor: 'maximumCapacity',
+            Cell: props => {
+                const countWaitlisted = this.props.courseRegistrations.filter(reg => reg.courseCode == props.original.courseCode && reg.registrationStatus == "Waitlisted").length
+                return <div style={countWaitlisted > 0 ? { color: 'orange', fontWeight: 'bold' } : { fontWeight: 'bold' }}>{countWaitlisted}</div>
+            }
         }, {
             Header: '',
             accessor: 'courseId',
