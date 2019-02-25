7851be9b5b9d42bea2518d8248694c96453b6be0
ssskram <paul.marks@pittsburghpa.gov>
2019-02-25 13:00:40 -0500

    if user has cancellation record for course, still able to reregister

--- app/src/components/courseRegistration/functions/userIsRegistered.ts
+++ app/src/components/courseRegistration/functions/userIsRegistered.ts
@@ -1,4 +1,5 @@
+
 export default function userIsRegistered(user, course, registrations) {
-    const registrationExists = registrations.some(reg => reg.courseCode == course.courseCode && reg.user == user.email)
+    const registrationExists = registrations.some(reg => reg.courseCode == course.courseCode && reg.user == user.email && reg.registrationStatus != "Canceled")
     return registrationExists
 }
--- app/src/components/courseRegistration/markup/table.tsx
+++ app/src/components/courseRegistration/markup/table.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import * as types from '../../../store/types'
 import ReactTable from "react-table"
 import "react-table/react-table.css"
-import * as moment from 'moment'
 
 type props = {
     courses: types.course[]
