d4d0e35108d439751683be4983c5c8ecff33ac4e
ssskram <paul.marks@pittsburghpa.gov>
2019-03-01 16:10:10 -0500

    returning course reg to staging

--- app/src/components/home/alerts.tsx
+++ app/src/components/home/alerts.tsx
@@ -5,13 +5,13 @@ export default class Alerts extends React.Component {
 
     render() {
         return <div>
-            {/* <div style={{ marginTop: '20px' }}>
+            <div style={{ marginTop: '20px' }}>
                 <div style={{ fontSize: '1.2em' }} className='alert alert-info'>
                     <div style={{ fontSize: '1.5em', padding: '5px' }}>Hungry to learn?!</div>
                     <div>Enrollment is now open for project management courses!</div>
                     <Link to={'/CourseRegistration'} className="alert-link">Click here to register now</Link>
                 </div>
-            </div> */}
+            </div>
             <div>
                 <div style={{ fontSize: '1.2em' }} className='alert alert-warning'>
                     New service!  <Link to={'/equipmentDisposal'} className="alert-link">Dispose of unused IT equipment</Link>
