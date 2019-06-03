e5c804468c4e06d196ee9413dbca9c4e3598ff07
ssskram <paul.marks@pittsburghpa.gov>
2019-06-03 11:20:11 -0400

    fixed isBetween filter for equipment loans

--- app/src/components/services/equipmentLoans/availableItems.ts
+++ app/src/components/services/equipmentLoans/availableItems.ts
@@ -1,40 +1,49 @@
-import * as moment from 'moment'
+import * as moment from "moment";
 
 // takes the timepsan provided, all equipment, and all existing reservations
 export function getAvailableEquipment(state, equipment, reservations) {
-
-    // finds existing reservations that overlap with timespan
-    const format = 'MM-DD-YYYY, hh:mm A'
-    const overlappingResos = reservations.filter(reso => {
-        return (moment(reso.from).isBetween(moment(state.fromDate + ' ' + state.fromTime).format(format), moment(state.toDate + ' ' + state.toTime).format(format))) || 
-            (moment(reso.to).isBetween(moment(state.fromDate + ' ' + state.fromTime).format(format), moment(state.toDate + ' ' + state.toTime).format(format)))
-    })
-    if (overlappingResos.length == 0) {
-        return equipment
-    } else {
-        // if reservations overlap with timepsan, then return all equipment sub equipment for overlapping reservations
-        let availableEquipment = equipment.filter(item => {
-            return overlappingResos.filter(reso => {
-                return reso.itemID == item.itemID
-            }).length == 0
-        })
-        return availableEquipment
-    }
+  // finds existing reservations that overlap with timespan
+  const format = "MM-DD-YYYY, hh:mm A";
+  const from = moment(state.fromDate + ", " + state.fromTime).format(format);
+  const to = moment(state.toDate + ", " + state.toTime).format(format);
+  const overlappingResos = reservations.filter(reso => {
+    return (
+      moment(reso.from).isBetween(from, to) ||
+      moment(reso.to).isBetween(from, to) ||
+      moment(from).isBetween(reso.from, reso.to) ||
+      moment(to).isBetween(reso.from, reso.to)
+    );
+  });
+  if (overlappingResos.length == 0) {
+    return equipment;
+  } else {
+    // if reservations overlap with timepsan, then return all equipment sub equipment for overlapping reservations
+    let availableEquipment = equipment.filter(item => {
+      return (
+        overlappingResos.filter(reso => {
+          return reso.itemID == item.itemID;
+        }).length == 0
+      );
+    });
+    return availableEquipment;
+  }
 }
 
 // takes an item type, array of available equipment from getAvailableEquipment(), and  flavor (accessory vs. nonaccessory)
 export function checkAvailability(itemType, availableEquipment, flavor) {
-    // filters available equipment by item type
-    if (flavor == 'Accessory') {
-        const available = availableEquipment.filter(item => item.item == itemType)
-        if (available.length > 0) {
-            // got anything? return true
-            return true
-        } else return false
-    } else {
-        const available = availableEquipment.filter(item => item.itemType == itemType)
-        if (available.length > 0) {
-            return true
-        } else return false
-    }
+  // filters available equipment by item type
+  if (flavor == "Accessory") {
+    const available = availableEquipment.filter(item => item.item == itemType);
+    if (available.length > 0) {
+      // got anything? return true
+      return true;
+    } else return false;
+  } else {
+    const available = availableEquipment.filter(
+      item => item.itemType == itemType
+    );
+    if (available.length > 0) {
+      return true;
+    } else return false;
+  }
 }
--- app/src/components/services/equipmentLoans/index.tsx
+++ app/src/components/services/equipmentLoans/index.tsx
@@ -1,213 +1,226 @@
-import * as React from 'react'
-import { Redirect } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { ApplicationState } from '../../../store'
-import * as types from '../../../store/types'
-import * as Loans from '../../../store/equipmentLoan'
-import * as Equipment from '../../../store/equipment'
-import * as Messages from '../../../store/messages'
-import * as User from '../../../store/user'
-import * as moment from 'moment'
-import { getAvailableEquipment } from './availableItems'
-import randomize from 'randomatic'
-import postOTRS from './postOTRS'
-import postSP from './postSP'
-import SelectTimeSpan from './markup/selectTimeSpan'
-import UserInfo from './markup/userInfo'
-import SelectEquipment from './markup/selectEquipment'
-import SelectAccessories from './markup/selectAccessories'
-import SubmitButton from '../shared/submitButton'
-import Header from '../shared/header'
+import * as React from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { ApplicationState } from "../../../store";
+import * as types from "../../../store/types";
+import * as Loans from "../../../store/equipmentLoan";
+import * as Equipment from "../../../store/equipment";
+import * as Messages from "../../../store/messages";
+import * as User from "../../../store/user";
+import * as moment from "moment";
+import { getAvailableEquipment } from "./availableItems";
+import randomize from "randomatic";
+import postOTRS from "./postOTRS";
+import postSP from "./postSP";
+import SelectTimeSpan from "./markup/selectTimeSpan";
+import UserInfo from "./markup/userInfo";
+import SelectEquipment from "./markup/selectEquipment";
+import SelectAccessories from "./markup/selectAccessories";
+import SubmitButton from "../shared/submitButton";
+import Header from "../shared/header";
 
 type props = {
-    loans: types.equipmentLoans
-    equipment: types.equipment
-    messages: types.messsage
-    user: types.user
-    loadEquipment: () => void
-    loadEquipmentLoans: () => void
-    newMessage: (message: string) => void
-    errorMessage: () => void
-}
+  loans: types.equipmentLoans;
+  equipment: types.equipment;
+  messages: types.messsage;
+  user: types.user;
+  loadEquipment: () => void;
+  loadEquipmentLoans: () => void;
+  newMessage: (message: string) => void;
+  errorMessage: () => void;
+};
 
 type state = {
-    reservationID: string
-    department: string
-    phone: string
-    items: types.equipmentItem[]
-    fromDate: string
-    fromTime: string
-    toDate: string
-    toTime: string
-    throwTimeError: boolean
-    selectedTypes: string[]
-    availableItems: types.equipmentItem[]
-    redirect: boolean
-}
+  reservationID: string;
+  department: string;
+  phone: string;
+  items: types.equipmentItem[];
+  fromDate: string;
+  fromTime: string;
+  toDate: string;
+  toTime: string;
+  throwTimeError: boolean;
+  selectedTypes: string[];
+  availableItems: types.equipmentItem[];
+  redirect: boolean;
+};
 
 export class EquipmentReservations extends React.Component<props, state> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            reservationID: randomize('0', 7),
-            department: '',
-            phone: '',
-            items: [],
-            fromDate: '',
-            fromTime: '',
-            toDate: '',
-            toTime: '',
-            throwTimeError: false,
-            selectedTypes: [],
-            availableItems: [],
-            redirect: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      reservationID: randomize("0", 7),
+      department: "",
+      phone: "",
+      items: [],
+      fromDate: "",
+      fromTime: "",
+      toDate: "",
+      toTime: "",
+      throwTimeError: false,
+      selectedTypes: [],
+      availableItems: [],
+      redirect: false
+    };
+  }
 
-    componentDidMount() {
-        window.scrollTo(0, 0)
-        this.props.loadEquipment()
-        this.props.loadEquipmentLoans()
-    }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.loadEquipment();
+    this.props.loadEquipmentLoans();
+  }
 
-    checkTimeFrame() {
-        // calculate span in hours
-        const hours = moment.duration(moment(this.state.toDate + ' ' + this.state.toTime).diff(moment(this.state.fromDate + ' ' + this.state.fromTime))).asHours()
-        // if desired time frame is greater than 72 hours
-        if (hours > 72) {
-            // clear fields, throw error
-            this.setState({ toTime: '', throwTimeError: true })
-        } else {
-            // calculate available items
-            this.setState({ throwTimeError: false, availableItems: getAvailableEquipment(this.state, this.props.equipment, this.props.loans) })
-        }
-    }
-
-    addRemoveItem(type, flavor) {
-        // manages state arrays of selectedTypes and items
-        const {
-            selectedTypes,
-            availableItems,
-            items
-        } = this.state
-
-        if (!selectedTypes.includes(type)) { // add item
-            // get an item of corresponding item type
-            const item: any = availableItems.find(item => item.itemType == type || item.item == type)
-            // add item to state, and add type to selectedTypes for reference
-            this.setState({
-                selectedTypes: [...this.state.selectedTypes, type],
-                items: [...this.state.items, item]
-            })
-        } else { // remove item
-            // filter out item & item type from existing state arrays
-            const filteredTypes = selectedTypes.filter(item => item != type)
-            let filteredItems = [] as any
-            if (flavor == 'Accessory') {
-                filteredItems = items.filter(item => item.item != type)
-            } else {
-                filteredItems = items.filter(item => item.itemType != type)
-            }
-            // set filtered arrays to state
-            this.setState({ selectedTypes: filteredTypes, items: filteredItems })
-        }
-    }
-
-    async submit() {
-        // data to post
-        const load = {
-            reservationID: this.state.reservationID,
-            department: this.state.department,
-            phone: this.state.phone,
-            from: this.state.fromDate + ' ' + this.state.fromTime,
-            to: this.state.toDate + ' ' + this.state.toTime,
-            items: this.state.items,
-            types: this.state.selectedTypes
-        }
-        // communicate success/failure
-        let success = true
-        success = await postSP(load, this.props.user)
-        if (success == true) success = await postOTRS(load, this.props.user)
-        if (success == true) {
-            this.props.newMessage('Success!<br/>Your reservation ID is ' + this.state.reservationID + '<br/>Please bring your reservation ID when picking up your equipment')
-        } else {
-            this.props.errorMessage()
-        }
-        // send 'em on home
-        this.setState({ redirect: true })
-    }
-
-    public render() {        
-        const showEquipment =
-            this.state.fromDate != '' &&
-            this.state.fromTime != '' &&
-            this.state.toDate != '' &&
-            this.state.toTime != '' &&
-            this.state.toTime != null &&
-            this.state.throwTimeError != true
-
-        const valid =
-            this.state.items.length > 0
-
-        if (this.state.redirect) {
-            return <Redirect to='/' />
-        }
-
-        return (
-            <div>
-                <Header
-                    mainText='Borrow equipment'
-                    subText='Complete all fields and submit'
-                />
-                <div className='col-md-12'>
-                    <UserInfo
-                        parentState={this.state}
-                        setState={this.setState.bind(this)}
-                    />
-                    <SelectTimeSpan
-                        fromDate={this.state.fromDate}
-                        fromTime={this.state.fromTime}
-                        toDate={this.state.toDate}
-                        toTime={this.state.toTime}
-                        throwTimeError={this.state.throwTimeError}
-                        setState={this.setState.bind(this)}
-                        checkTimeFrame={this.checkTimeFrame.bind(this)}
-                    />
-                    {showEquipment == true &&
-                        <SelectEquipment
-                            parentState={this.state}
-                            addRemoveItem={this.addRemoveItem.bind(this)}
-                        />
-                    }
-                    {valid == true &&
-                        <div>
-                            <SelectAccessories
-                                parentState={this.state}
-                                addRemoveItem={this.addRemoveItem.bind(this)}
-                            />
-                        </div>
-                    }
-                    <SubmitButton
-                        isEnabled={valid}
-                        submit={this.submit.bind(this)}
-                    />
-                </div>
-            </div>
+  checkTimeFrame() {
+    // calculate span in hours
+    const hours = moment
+      .duration(
+        moment(this.state.toDate + " " + this.state.toTime).diff(
+          moment(this.state.fromDate + " " + this.state.fromTime)
         )
+      )
+      .asHours();
+    // if desired time frame is greater than 72 hours
+    if (hours > 72) {
+      // clear fields, throw error
+      this.setState({ toTime: "", throwTimeError: true });
+    } else {
+      // calculate available items
+      this.setState({
+        throwTimeError: false,
+        availableItems: getAvailableEquipment(
+          this.state,
+          this.props.equipment,
+          this.props.loans
+        )
+      });
     }
+  }
+
+  addRemoveItem(type, flavor) {
+    // manages state arrays of selectedTypes and items
+    const { selectedTypes, availableItems, items } = this.state;
+
+    if (!selectedTypes.includes(type)) {
+      // add item
+      // get an item of corresponding item type
+      const item: any = availableItems.find(
+        item => item.itemType == type || item.item == type
+      );
+      // add item to state, and add type to selectedTypes for reference
+      this.setState({
+        selectedTypes: [...this.state.selectedTypes, type],
+        items: [...this.state.items, item]
+      });
+    } else {
+      // remove item
+      // filter out item & item type from existing state arrays
+      const filteredTypes = selectedTypes.filter(item => item != type);
+      let filteredItems = [] as any;
+      if (flavor == "Accessory") {
+        filteredItems = items.filter(item => item.item != type);
+      } else {
+        filteredItems = items.filter(item => item.itemType != type);
+      }
+      // set filtered arrays to state
+      this.setState({ selectedTypes: filteredTypes, items: filteredItems });
+    }
+  }
+
+  async submit() {
+    // data to post
+    const load = {
+      reservationID: this.state.reservationID,
+      department: this.state.department,
+      phone: this.state.phone,
+      from: this.state.fromDate + " " + this.state.fromTime,
+      to: this.state.toDate + " " + this.state.toTime,
+      items: this.state.items,
+      types: this.state.selectedTypes
+    };
+    // communicate success/failure
+    let success = true;
+    success = await postSP(load, this.props.user);
+    if (success == true) success = await postOTRS(load, this.props.user);
+    if (success == true) {
+      this.props.newMessage(
+        "Success!<br/>Your reservation ID is " +
+          this.state.reservationID +
+          "<br/>Please bring your reservation ID when picking up your equipment"
+      );
+    } else {
+      this.props.errorMessage();
+    }
+    // send 'em on home
+    this.setState({ redirect: true });
+  }
+
+  public render() {
+    const showEquipment =
+      this.state.fromDate != "" &&
+      this.state.fromTime != "" &&
+      this.state.toDate != "" &&
+      this.state.toTime != "" &&
+      this.state.toTime != null &&
+      this.state.throwTimeError != true;
+
+    const valid = this.state.items.length > 0;
+
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+
+    return (
+      <div>
+        <Header
+          mainText="Borrow equipment"
+          subText="Complete all fields and submit"
+        />
+        <div className="col-md-12">
+          <UserInfo
+            parentState={this.state}
+            setState={this.setState.bind(this)}
+          />
+          <SelectTimeSpan
+            fromDate={this.state.fromDate}
+            fromTime={this.state.fromTime}
+            toDate={this.state.toDate}
+            toTime={this.state.toTime}
+            throwTimeError={this.state.throwTimeError}
+            setState={this.setState.bind(this)}
+            checkTimeFrame={this.checkTimeFrame.bind(this)}
+          />
+          {showEquipment == true && (
+            <SelectEquipment
+              parentState={this.state}
+              addRemoveItem={this.addRemoveItem.bind(this)}
+            />
+          )}
+          {valid == true && (
+            <div>
+              <SelectAccessories
+                parentState={this.state}
+                addRemoveItem={this.addRemoveItem.bind(this)}
+              />
+            </div>
+          )}
+          <SubmitButton isEnabled={valid} submit={this.submit.bind(this)} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(
-    (state: ApplicationState) => ({
-        ...state.loans,
-        ...state.messages,
-        ...state.equipment,
-        ...state.user
-    }),
-    ({
-        ...Loans.actionCreators,
-        ...Messages.actionCreators,
-        ...Equipment.actionCreators,
-        ...User.actionCreators
-    })
-)(EquipmentReservations as any)
+  (state: ApplicationState) => ({
+    ...state.loans,
+    ...state.messages,
+    ...state.equipment,
+    ...state.user
+  }),
+  {
+    ...Loans.actionCreators,
+    ...Messages.actionCreators,
+    ...Equipment.actionCreators,
+    ...User.actionCreators
+  }
+)(EquipmentReservations as any);
